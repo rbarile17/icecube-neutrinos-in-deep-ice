1f21ff1b4ee6c07c4d5b2849ef440c07d5858730
Roberto Barile <r.barile17@studenti.uniba.it>
2023-03-16 12:50:17 +0100

    fix data paths

--- src/data/paths.py
+++ src/data/paths.py
@@ -3,5 +3,5 @@ from pathlib import Path
 DATA_PATH = Path('data')
 
 RAW_DATA_PATH = DATA_PATH / 'raw'
-RAW_TRAIN_BATCHES_PATH = DATA_PATH / 'train'
-RAW_META_PATH = DATA_PATH / 'meta'
+RAW_TRAIN_BATCHES_PATH = RAW_DATA_PATH / 'train'
+RAW_META_PATH = RAW_DATA_PATH / 'meta'
