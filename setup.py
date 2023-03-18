a6fcbcbb0bc883868fe642f26478fc378a670775
Roberto Barile <r.barile17@studenti.uniba.it>
2023-03-18 16:26:38 +0100

    testing config

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Development of a model for the reconstruction of the direction of neutrinos from the IceCube Neutrino Observatory.',
+    author='Roberto Barile',
+    license='MIT',
+)
