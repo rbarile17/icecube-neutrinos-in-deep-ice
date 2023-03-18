04da9db391d115cf1203eebf92027d8ad4a9e0de
Roberto Barile <r.barile17@studenti.uniba.it>
2023-03-18 16:27:28 +0100

    basic unit tests

---
+++ src/tests/test_model.py
@@ -0,0 +1,40 @@
+import pytest
+import torch
+
+from torch_geometric.data import Data, Batch
+
+from src.models import DynEdge
+from src.data import IceCube
+
+
+@pytest.fixture
+def model():
+    return DynEdge()
+
+
+@pytest.fixture
+def dataset():
+    return IceCube([1], batch_size=100)
+
+
+def test_train_dynedge(model, dataset):
+    model = DynEdge()
+
+    losses = []
+    for i, example in enumerate(dataset):
+        losses.append(model.training_step(example, None, log=False))
+
+        if i == 10:
+            break
+
+    assert losses[-1] < losses[0]
+
+
+@torch.no_grad()
+def test_shape(model, dataset):
+    dataset_iterator = iter(dataset)
+    example = next(dataset_iterator)
+    pred = model(example)
+    assert pred.shape == (100, 4)
+
+
