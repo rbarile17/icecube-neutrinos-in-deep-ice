1dcd7b29c63cc0b05059851e7c9f4505de7dbe07
Roberto Barile <r.barile17@studenti.uniba.it>
2023-03-29 11:01:06 +0200

    simplified and faster 3d loss

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,5 @@
 """Utility functions and classes for the project."""
 
 from .utils import angle_to_xyz, xyz_to_angle, angular_error, eps_like
-from .loss_functions import VonMisesFisher2DLoss, VonMisesFisher3DLoss
+from .loss_functions import VonMisesFisher2DLoss
+from .loss_functions import vmf_3d_loss
