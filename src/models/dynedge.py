c4ffd94fd6bc06d2e29eba0ac88826a1fd725493
Roberto Barile <r.barile17@studenti.uniba.it>
2023-04-01 18:18:02 +0200

    fix integration of lstm

--- src/models/dynedge.py
+++ src/models/dynedge.py
@@ -12,6 +12,7 @@ from torch import nn
 from torch_geometric.nn import knn_graph, EdgeConv
 from torch_geometric.utils import homophily
 from torch_geometric.data import Batch
+from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence
 from torch_scatter import scatter_mean, scatter_max, scatter_min
 
 from ..utils import VonMisesFisher2DLoss, VonMisesFisher3DLoss
@@ -38,14 +39,19 @@ class DynEdge(pl.LightningModule):
         super().__init__()
         self.save_hyperparameters()
 
-        self.lstm = nn.LSTM(6, 32, 4, bidirectional=True)
+        self.lstm = nn.LSTM(
+            input_size=17, 
+            hidden_size=32,
+            num_layers=1
+        )
 
         self.conv0 = EdgeConv(MLP([34, 128, 256]), aggr='add')
         self.conv1 = EdgeConv(MLP([512, 336, 256]), aggr='add')
         self.conv2 = EdgeConv(MLP([512, 336, 256]), aggr='add')
         self.conv3 = EdgeConv(MLP([512, 336, 256]), aggr='add')
-        self.post = MLP([1105, 336, 256])
-        self.readout = MLP([768, 128])
+        self.post = MLP([1041, 336, 256])
+        self.readout_gnn = MLP([768, 128])
+        self.readout = MLP([160, 128])
 
     # pylint: disable=arguments-differ
     def forward(self, data: Batch):
@@ -57,8 +63,6 @@ class DynEdge(pl.LightningModule):
         vert_feat[:, 2] /= 500.0  # z
         vert_feat[:, 3] = (vert_feat[:, 3] - 1.0e04) / 3.0e4  # time
         vert_feat[:, 4] = torch.log10(vert_feat[:, 4]) / 3.0  # charge
-
-        lstm_out = self.lstm(vert_feat)[0]
 
         edge_index = knn_graph(vert_feat[:, :3], 8, data.batch)
 
@@ -73,9 +77,16 @@ class DynEdge(pl.LightningModule):
         ], dim=1)  # [B, 11]
 
         # Distribute global_feats to each vertex
-        _, cnts = torch.unique_consecutive(data.batch, return_counts=True)
-        global_feats = torch.repeat_interleave(global_feats, cnts, dim=0)
+        _, counts = torch.unique_consecutive(data.batch, return_counts=True)
+        global_feats = torch.repeat_interleave(global_feats, counts, dim=0)
         vert_feat = torch.cat((vert_feat, global_feats), dim=1)
+
+        vert_feat_lstm = torch.split(vert_feat, counts.tolist(), dim=0)
+        vert_feat_lstm = pad_sequence(vert_feat_lstm, batch_first=True)
+        vert_feat_lstm = pack_padded_sequence(vert_feat_lstm, counts.tolist(), batch_first=True, enforce_sorted=False)
+
+        packed_output = self.lstm(vert_feat_lstm)[0]
+        lstm_out = torch.stack([event[-1] for event in torch.split(packed_output.data, counts.tolist(), dim=0)])
 
         # Convolutions
         feats = [vert_feat]
@@ -97,11 +108,10 @@ class DynEdge(pl.LightningModule):
 
         # Postprocessing
         post_inp = torch.cat(feats, dim=1)
-        post_inp = torch.cat([post_inp, lstm_out], dim=1)
         post_out = self.post(post_inp)
 
         # Readout
-        readout_inp = torch.cat(
+        readout_gnn_inp = torch.cat(
             [
                 scatter_min(post_out, data.batch, dim=0)[0],
                 scatter_max(post_out, data.batch, dim=0)[0],
@@ -109,6 +119,8 @@ class DynEdge(pl.LightningModule):
             ],
             dim=1,
         )
+        readout_gnn_out = self.readout_gnn(readout_gnn_inp)
+        readout_inp = torch.cat([readout_gnn_out, lstm_out], dim=1)
         readout_out = self.readout(readout_inp)
 
         return readout_out
@@ -179,8 +191,8 @@ class DynEdge3DLoss(DynEdge):
         pred_xyz = self.forward(data)  # [B, 4]
         true_xyz = data.gt.view(-1, 3)  # [B, 3]
 
-        loss = VonMisesFisher3DLoss()(pred_xyz, true_xyz)
-        error = angular_error(pred_xyz, true_xyz).mean()
+        loss = VonMisesFisher3DLoss()(pred_xyz, true_xyz).mean()
+        error = angular_error(pred_xyz[:, :3], true_xyz).mean()
 
         if log:
             self.log(f'loss/{prefix}', loss, batch_size=len(true_xyz))
