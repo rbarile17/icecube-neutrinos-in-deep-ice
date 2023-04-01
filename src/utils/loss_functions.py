178634cde886db3a0b0f90b3d73cdc1029205554
Roberto Barile <r.barile17@studenti.uniba.it>
2023-04-01 09:40:18 +0200

    rollback 3d loss

--- src/utils/loss_functions.py
+++ src/utils/loss_functions.py
@@ -129,9 +129,29 @@ class VonMisesFisher2DLoss(VonMisesFisherLoss):
         )
 
         return self._evaluate(p, t)
-    
 
-def vmf_3d_loss(pred, target, eps = 1e-8):
-    kappa = torch.norm(pred, dim=1)        
-    logC  = -kappa + torch.log((kappa + eps) /(1 - torch.exp(-2 * kappa) + 2 * eps))
-    return -((target * pred).sum(dim=1) + logC).mean() 
+
+class VonMisesFisher3DLoss(VonMisesFisherLoss):
+    """von Mises-Fisher loss function vectors in the 2D plane."""
+
+    def forward(self, prediction: Tensor, target: Tensor) -> Tensor:
+        """Calculate von Mises-Fisher loss for a direction in the 3D.
+
+        Args:
+            prediction: Output of the model. Must have shape [N, 4] where
+                columns 0, 1, 2 are predictions of `direction` and last column
+                is an estimate of `kappa`.
+            target: Target tensor, extracted from graph object.
+
+        Returns:
+            Elementwise von Mises-Fisher loss terms. Shape [N,]
+        """
+        target = target.reshape(-1, 3)
+        # Check(s)
+        assert prediction.dim() == 2 and prediction.size()[1] == 4
+        assert target.dim() == 2
+        assert prediction.size()[0] == target.size()[0]
+
+        kappa = prediction[:, 3]
+        p = kappa.unsqueeze(1) * prediction[:, [0, 1, 2]]
+        return self._evaluate(p, target)
